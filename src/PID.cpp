30d29ce9b0646df87a11ac1a319a80d7acdd39ca
Dhatri Chennavajula <chdhatri.b@gmail.com>
2018-03-10 15:56:14 +0530

    Changes to the PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <limits>
 
 using namespace std;
 
@@ -11,11 +12,33 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    
+    //Intialize co-efficients
+    
+    //intialize errors
+    //setting the p_error
+    p_error = 0.0;//numeric_limits<double>::max();
+    i_error = 0.0;
+    d_error = 0.0;
+    
+    this->Kp = Kp; // propotional coeff
+    this->Ki = Ki; //Integral coeff
+    this->Kd = Kd; //Differntail coeff
+    
+  
+    
 }
 
 void PID::UpdateError(double cte) {
+   
+
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error += cte;
+   
 }
 
 double PID::TotalError() {
+    return Kp * p_error + Ki * i_error + Kd * d_error;
 }
 
